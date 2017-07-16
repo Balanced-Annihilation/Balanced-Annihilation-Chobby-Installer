5647e4292256343052eb9334ff78699577bdb955
tzaeru <tzaeru@gmail.com>
2017-07-16 20:19:34 +0300

    Initial work - setting up directories and subprojects

---
+++ launcher/Balanced Annihilation Launcher/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Balanced_Annihilation_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.updateProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // updateProgress
+            // 
+            this.updateProgress.Location = new System.Drawing.Point(49, 272);
+            this.updateProgress.Name = "updateProgress";
+            this.updateProgress.Size = new System.Drawing.Size(366, 23);
+            this.updateProgress.TabIndex = 0;
+            this.updateProgress.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 355);
+            this.Controls.Add(this.updateProgress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar updateProgress;
+    }
+}
+
---
+++ launcher/Balanced Annihilation Launcher/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Balanced_Annihilation_Launcher
+{
+    public partial class Form1 : Form
+    {
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [SuppressUnmanagedCodeSecurity, DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool PeekMessage(out Message msg, IntPtr hWnd, uint messageFilterMin, uint messageFilterMax, uint flags);
+
+        public Form1()
+        {
+            Application.Idle += new EventHandler(Application_Idle);
+            InitializeComponent();
+            
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            Message message;
+            while (!PeekMessage(out message, IntPtr.Zero, 0, 0, 0))
+            {
+                updateProgress.Value = new Random().Next(0, 100);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("CLICKED");
+        }
+    }
+}
